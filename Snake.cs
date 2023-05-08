173f3f6147be47a9811f81c8b5186f0c93cc9c9e
opilane <opilane@HP-CZC2349HTB>
2023-05-08 11:31:17 +0300

    1.2

--- Snake.cs
+++ Snake.cs
@@ -10,7 +10,12 @@ namespace UssMang
     {
         public Snake(Point tail, int length, Direction direction)
         {
-
+            for (int i = 0; i < length; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                pList.Add(p);
+            }
         }
     }
 }
