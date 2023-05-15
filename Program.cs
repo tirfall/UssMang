d2b3fb5f7e8ee24185e9e03e920de98d0a7a3946
opilane <opilane@HP-CZC2349HTB>
2023-05-15 15:11:07 +0300

    1.9

--- Program.cs
+++ Program.cs
@@ -14,10 +14,6 @@ namespace UssMang
         {
             Console.SetWindowSize(120, 30);
             //рамка
-            //HorizontalLine xline = new HorizontalLine(0,119,0,'#',29);
-            //xline.Draw();
-            //VerLine yline = new VerLine(0, 29, 0, '#', 119);
-            //yline.Draw();
 
             Walls walls = new Walls(119, 30);
             walls.Draw();
@@ -31,30 +27,57 @@ namespace UssMang
             Point food = foodCreator.CreateFood();
             food.Draw();
 
-            while(true)
+            while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
                     food.Draw();
                 }
-                else { snake.Move(); }
-                Thread.Sleep(100);
+                else
+                {
+                    snake.Move();
+                }
 
-                if(Console.KeyAvailable)
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
             }
-
+            WriteGameOver();
+            Console.ReadLine();
 
             //Sound mäng = new Sound();
             //ConsoleKeyInfo nupp= new ConsoleKeyInfo();
             //_= mäng.Heli("../../../.mp3")
             //
 
-            
+
+        }
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("GAME OVER", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("Sisesta sinu nimi:", xOffset + 2, yOffset++);
+            WriteText("", xOffset + 1, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
